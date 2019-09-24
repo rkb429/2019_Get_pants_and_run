0e4a322f9343db50a9c7c93dcbacb6a0d31f39cf
organicnameko <file0429@naver.com>
2019-09-24 18:53:58 +0900

    피격판정

---
+++ 봉국/Assets/script/hpsystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpsystem : MonoBehaviour
+{
+    public int maxhp = 5;
+    public int hp;
+    public GameObject MainObject;
+    public Text hptext;
+    // Use this for initialization
+    void Start()
+    {
+        hp = maxhp;
+        Debug.Log("hp:" + hp);
+        hptext.text = hp.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("obstacle"))
+        {
+            Debug.Log("충돌");
+            hp -= 1;
+            Debug.Log("hp:" + hp);
+            hptext.text = hp.ToString();
+        }
+    }
+}
